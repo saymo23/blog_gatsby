2e6966118dd4ec0b6c1f56cc19b55dde1dbddd42
Daniel Santarriaga <daniel.santarriaga@gmail.com>
2021-01-26 22:01:09 -0700

    first commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,10 +21,25 @@ module.exports = {
         name: `gatsby-starter-default`,
         short_name: `starter`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#F6AD55`,
+        theme_color: `#F6AD55`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `pages`,
+        path: `${__dirname}/src/pages/`,
+      },
+    },
+    {
+        resolve: `gatsby-plugin-mdx`,
+        options: {
+          defaultLayouts: {
+            default: require.resolve(`./src/components/layout.js`),
+        },
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
--- src/components/layout.js
+++ src/components/layout.js
@@ -8,6 +8,7 @@
 import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
+import SEO from "../components/seo"
 
 import Header from "./header"
 import "./layout.css"
@@ -25,6 +26,7 @@ const Layout = ({ children }) => {
 
   return (
     <>
+      <SEO title="Home" />
       <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
       <div
         style={{
